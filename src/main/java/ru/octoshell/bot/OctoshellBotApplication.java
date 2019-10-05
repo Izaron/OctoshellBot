4813d3abaa39872f300d6a87cebec9099858a0b6
Izaron <izaronplatz@gmail.com>
2019-10-05 19:00:13 +0300

    Initial commit

---
+++ src/main/java/ru/octoshell/bot/OctoshellBotApplication.java
@@ -0,0 +1,13 @@
+package ru.octoshell.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoshellBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoshellBotApplication.class, args);
+	}
+
+}
