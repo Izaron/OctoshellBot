d7e0018ec6979aee9a263c98f1939cc7ab38a247
Izaron <izaronplatz@gmail.com>
2019-12-08 17:56:53 +0300

    Add updates info from Octoshell

--- src/main/java/ru/octoshell/bot/model/repository/BotLinkDataRepository.java
+++ src/main/java/ru/octoshell/bot/model/repository/BotLinkDataRepository.java
@@ -3,5 +3,9 @@ package ru.octoshell.bot.model.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ru.octoshell.bot.model.BotLinkData;
 
+import java.util.Optional;
+
 public interface BotLinkDataRepository extends MongoRepository<BotLinkData, Integer> {
+
+    Optional<BotLinkData> findByEmail(String email);
 }
--- src/main/java/ru/octoshell/bot/ws/WebController.java
+++ src/main/java/ru/octoshell/bot/ws/WebController.java
@@ -1,28 +1,85 @@
 package ru.octoshell.bot.ws;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.octoshell.bot.model.BotLinkData;
+import ru.octoshell.bot.model.repository.BotLinkDataRepository;
+import ru.octoshell.bot.service.LocaleService;
+import ru.octoshell.bot.service.OctoshellTelegramBot;
+import ru.octoshell.bot.service.statemachine.UserStateService;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @RestController
 public class WebController {
 
-    private AtomicInteger value = new AtomicInteger();
+    private final BotLinkDataRepository botLinkDataRepository;
+    private final OctoshellTelegramBot octoshellTelegramBot;
+    private final UserStateService userStateService;
+    private final LocaleService localeService;
 
-    @RequestMapping(value = "/value", method = RequestMethod.GET)
-    public String getValue() {
-        log.info("GET value");
-        return StringUtils.join("The value is ", value);
+    public WebController(BotLinkDataRepository botLinkDataRepository,
+                         OctoshellTelegramBot octoshellTelegramBot,
+                         UserStateService userStateService,
+                         LocaleService localeService) {
+        this.botLinkDataRepository = botLinkDataRepository;
+        this.octoshellTelegramBot = octoshellTelegramBot;
+        this.userStateService = userStateService;
+        this.localeService = localeService;
     }
 
-    @RequestMapping(value = "/value", method = RequestMethod.POST)
-    public void postValue() {
-        log.info("POST value");
-        value.incrementAndGet();
+    @PostMapping("/notify/ticket")
+    public void notifyTicket(@RequestBody Map<String, String> body) {
+        new Thread(new Sender(body)).start();
+    }
+
+    private class Sender implements Runnable {
+
+        private final Map<String, String> body;
+
+        private Sender(Map<String, String> body) {
+            this.body = body;
+        }
+
+        @Override
+        public void run() {
+            Optional<BotLinkData> botLinkDataOptional = botLinkDataRepository.findByEmail(body.get("email"));
+
+            if (botLinkDataOptional.isPresent()) {
+                BotLinkData botLinkData = botLinkDataOptional.get();
+                Integer userId = botLinkData.getUserId();
+                sendNotification(body.get("event"), body.get("subject"), userId);
+            }
+
+            log.info(body.toString());
+        }
+
+        private void sendNotification(String event, String ticketSubject, Integer userId) {
+            String locale = userStateService.getUserLocale(userId);
+
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(userId.toString());
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(localeService.getProperty(locale, "notify.ticket.header")).append("\n");
+            sb.append(localeService.getProperty(locale, "notify.ticket.subject"))
+                    .append(": \"").append(ticketSubject).append("\"\n");
+            sb.append(localeService.getProperty(locale, "notify.ticket.status." + event))
+                    .append("\n");
+
+            sendMessage.setText(sb.toString());
+
+            try {
+                octoshellTelegramBot.send(sendMessage);
+            } catch (TelegramApiException e) {
+                log.error(e.toString());
+            }
+        }
     }
 }
